15669ad8b533cd0a851559f9ab0835e1dad76226
OQVA Digital <116745354+OQVA-Digital@users.noreply.github.com>
2023-06-14 14:23:16 -0300

    Add files via upload

--- scripts/identity-framework.js
+++ scripts/identity-framework.js
@@ -2,19 +2,35 @@ let score;
 
 let answers = document.querySelectorAll('.answer input[type="range"]');
 
-let allFilled = false;
+let allFilled = true;
+
+let resultCtnr = document.querySelector('.result');
+let resultCtnrOffset;
+
+let maxScore = 0;
+
+for (i = 0; i < answers.length; i++) {
+
+    maxScore += Number(answers[i].getAttribute('max'))
+
+    if (i == answers.length - 1) {
+        console.log(maxScore + " is the max score")
+
+        document.querySelector('.score .total').innerHTML = '/ ' + maxScore
+    }
+}
 
 function calculateScore() {
 
-    for (i = 0; i < answers.length; i++) {
-        if (answers[i].classList.contains('unset')) {
-            alert("Please, answer all the fields.");
-            allFilled = false;
-            return
-        } else {
-            allFilled = true;
-        }
-    }
+    // for (i = 0; i < answers.length; i++) {
+    //     if (answers[i].classList.contains('unset')) {
+    //         alert("Please, answer all the fields.");
+    //         allFilled = false;
+    //         return
+    //     } else {
+    //         allFilled = true;
+    //     }
+    // }
 
     if (allFilled == true) {
         score = 0;
@@ -23,7 +39,14 @@ function calculateScore() {
             score += Number(answers[i].value)
         }
 
-        alert(score + " points")
+        resultCtnr.classList.remove('hidden')
+        resultCtnrOffset = resultCtnr.offsetTop
+
+        document.querySelector('.score .current').innerHTML = score
+
+        setTimeout(() => {
+            window.scrollTo(0, document.body.scrollHeight)
+        }, 900);
     }
 
     // if (answers) {
@@ -36,13 +59,18 @@ function calculateScore() {
     // }
 }
 
-function updateThumb() {
-    this.classList.remove('unset')
-    this.setAttribute('value', this.value)
+function updateThumb(event) {
+    const element = event.target;
+    element.classList.remove('unset');
+    element.setAttribute('value', element.value);
 }
 
-for (i = 0; i < answers.length; i++) {
-    answers[i].addEventListener("input", updateThumb);
+for (let i = 0; i < answers.length; i++) {
+    answers[i].addEventListener("input", (event) => {
+        updateThumb(event)
+        resultCtnr.classList.add('hidden');
+    });
+
     answers[i].addEventListener("mousedown", updateThumb);
     answers[i].addEventListener("touchstart", updateThumb);
 }
