7988439f2e9e8b3a1eae53a4aa4faa5bbf8c9d00
OQVA Digital <116745354+OQVA-Digital@users.noreply.github.com>
2023-06-14 17:38:59 -0300

    Add files via upload

--- scripts/form.js
+++ scripts/form.js
@@ -5,25 +5,25 @@
 const allInputs = document.querySelectorAll('input, textarea')
 
 const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-    
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => success())
-      .catch((error) => alert(error));
-  };
-  
+  event.preventDefault();
 
-  document.querySelector("form").addEventListener("submit", handleSubmit);
+  const myForm = event.target;
+  const formData = new FormData(myForm);
 
-  const formBt = document.querySelector('form button')
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => success())
+    .catch((error) => alert(error));
+};
+
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const formBt = document.querySelector('form button[type="submit"]')
 
 function success() {
-  formBt.innerHTML = 'Form successfully sent. Thanks!'
+  formBt.innerHTML = 'Successfully sent. Thanks!'
 }
--- scripts/identity-framework.js
+++ scripts/identity-framework.js
@@ -9,12 +9,19 @@ let resultCtnrOffset;
 
 let maxScore = 0;
 
+const resultText = document.querySelector('.result .general.txt')
+
 for (i = 0; i < answers.length; i++) {
 
     maxScore += Number(answers[i].getAttribute('max'))
 
     if (i == answers.length - 1) {
         console.log(maxScore + " is the max score")
+        console.log(maxScore / 2)
+        console.log(maxScore / 3)
+        console.log(maxScore / 4)
+        console.log(maxScore / 5)
+        console.log(maxScore / 6)
 
         document.querySelector('.score .total').innerHTML = '/ ' + maxScore
     }
@@ -40,23 +47,25 @@ function calculateScore() {
         }
 
         resultCtnr.classList.remove('hidden')
+        resultCtnr.style.maxHeight = resultCtnr.scrollHeight + 100 + 'px'
         resultCtnrOffset = resultCtnr.offsetTop
 
         document.querySelector('.score .current').innerHTML = score
 
         setTimeout(() => {
-            window.scrollTo(0, document.body.scrollHeight)
+            window.scrollTo(0, window.scrollY + (window.innerHeight / 2))
         }, 400);
+
+        if (score < 60) {
+            resultText.innerHTML = "<p>Your brand is in the early stages of developing its consciousness. <b>Don't worry - with focus and effort, you can build a strong brand consciousness.</b></p>"
+        } else if (score >= 60 && score <= 89) {
+            resultText.innerHTML = "<p>Your brand is on its way to <b>developing consciousness</b>, but there are many areas to improve. <b>A structured approach could help strengthen your brand's identity.</b></p>"
+        } else if (score >= 90 && score <= 119) {
+            resultText.innerHTML = "<p>Your brand has a <b>good level of consciousness</b>, but there's still room for improvement. <b>Consider focusing on areas where your score was lower.</b></p>"
+        } else if (score >= 120) {
+            resultText.innerHTML = "<p>Your brand has a <b>strong consciousness</b> and a <b>loyal tribe.</b></p>"
+        }
     }
-
-    // if (answers) {
-    //     score = parseInt(answers.value);
-
-    //     // Perform further calculations or actions based on the score
-    //     console.log("Selected score: " + score);
-    // } else {
-    //     console.log("Please select the answers.");
-    // }
 }
 
 function updateThumb(event) {
@@ -69,6 +78,7 @@ for (let i = 0; i < answers.length; i++) {
     answers[i].addEventListener("input", (event) => {
         updateThumb(event)
         resultCtnr.classList.add('hidden');
+        resultCtnr.style.maxHeight = '0'
     });
 
     answers[i].addEventListener("mousedown", updateThumb);
